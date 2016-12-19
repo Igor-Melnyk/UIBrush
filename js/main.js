eebacf7532d7ede75d5a8b6ab054e8fa036b0424
Мельник Ігор Віталійович <MelnikIV@xbank.com.ua>
2016-12-19 14:33:35 +0200

    add settings for mobile view

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $('document').ready( function () {
+	$('.mobile-nav').click(function () {
+		if ($('.top-navigation').css('display') == 'none'){
+			$('.top-navigation').slideDown(200);
+		} else {
+			$('.top-navigation').slideUp(200);
+		}
+	});
 
 });
